83aaf7d07bc11ec37c3650de7a6a74acf14e7e03
keremalpdemiroz <keremalpde@gmail.com>
2025-12-16 13:41:35 +0300

    feat: add authentication middleware and update jsonwebtoken dependency

---
+++ backend/auth/auth_check.js
@@ -0,0 +1,22 @@
+const {User} = require("../models/User");
+const jsonWebToken = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+require("dotenv").config();
+
+async function authMiddleware(req, res, next) {
+  const auth = req.headers.authorization;
+  if (!auth || !auth.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const token = auth.split(" ")[1];
+  try {
+    const decoded = jsonWebToken.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById({id:decoded.id});
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+}
+
+module.exports = authMiddleware;
